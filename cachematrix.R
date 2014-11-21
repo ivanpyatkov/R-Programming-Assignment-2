2da3bb9188d76bb062bce26125477ad034e97eee
ivanpyatkov <ivanpyatkov@gmail.com>
2014-11-21 10:29:26 +0200

    Preparing 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,42 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This functions compute the inverse of the special "matrix"
+## and creates a special "matrix" object that can cache its inverse.
+## If the inverse has already been calculated (and the matrix has not changed),
+## then the cachesolve should retrieve the inverse from the cache.
 
-## Write a short comment describing this function
+## makeCacheMatrix() is passed a matrix which creates an "object" of type 'list'.  
+## There are four functions, two to read (or 'get') the value of the two things we are storing, 
+## and two functions to change ('set') them.
 
-makeCacheMatrix <- function(x = matrix()) {
-
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
-
-
-makeCacheMatrix <- function(x = matrix()) {
-        m <- NULL
-        set <- function(y) {
+makeCacheMatrix <- function(x = matrix()) {     # input x will be a matrix
+        m <- NULL                               # m will be inverse matrix
+        set <- function(y) {                    # set the original matrix
                 x <<- y
                 m <<- NULL
         }
-        get <- function() x
-        setmean <- function(solve) m <<- solve
-        getmean <- function() m
-        list(set = set, get = get,
-             setmean = setmean,
-             getmean = getmean)
+        get <- function() x                     # this function returns the original matrix
+        
+        setsolve <- function(solve) m <<- solve # this is called during the first cacheSolve()
+        
+        getsolve <- function() m                # this will return the cached value to cacheSolve()
+        
+        list(set = set, get = get,... =         # this is accessed each time makeCacheMatrix() is called
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
 
-cacheSolve <- function(x, ...) {
-        m <- x$getmean()
-        if(!is.null(m)) {
-                message("getting cached data")
-                return(m)
+
+## cacheSolve() accesses the object (not the makeCacheMatrix() function, but the object
+## created when makeCacheMatrix() was called) by fetching the value of the matrix used
+## to create the object, this matrix being stored when the object was created.
+
+cacheSolve <- function(x, ...) {                # the input x is an object created by makeCacheMatrix
+        
+        m <- x$getsolve()                       # accesses the object 'x' and gets the value of the solve
+        
+        if(!is.null(m)) {                       # if mean was already cached (not NULL) ...
+                message("Getting cached data...")
+                return(m)                       # ... return the inverse matrix ... "return" ends 
         }
-        data <- x$get()
-        m <- solve(data, ...)
-        x$setmean(m)
-        m
+        data <- x$get()                         # we reach this code only if x$getsolve() returned NULL
+        
+        m <- solve(data, ...)                   # if m was NULL then we have to calculate the solve
+        
+        x$setsolve(m)                           # store the calculated solve value in x
+        m                                       # return the solve to the code that called this function
 }
+
